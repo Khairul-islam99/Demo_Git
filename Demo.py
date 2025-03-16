23a0d0041830f24032f0ce751b588b73c6335f78
khairul <khairulbdboss20031@gmail.com>
2025-03-16 17:25:55 +0600

    V4

--- Demo.py
+++ Demo.py
@@ -9,4 +9,12 @@ sum = num1 + num2
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print("Demo v2")
-print("Demo v3")
+print("Demo v4")
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
